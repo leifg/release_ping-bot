bcef621e09529c7e3b9bbbff3d4caf291998896e
Leif Gensert <leif@leif.io>
2017-10-25 23:21:53 +0200

    tweet to terminal

--- tweet.rb
+++ tweet.rb
@@ -1,7 +1,12 @@
-puts ARGV.inspect
-
-puts "whatever is going on"
+require "json"
 
 input = ARGF.read
 
-puts "Output the input: ##{input}#"
+if input == ""
+  $stderr.puts "Empty input"
+else
+  payload = JSON.parse(input, symbolize_names: true)
+
+  twitter_message = "#{payload[:software][:name]} #{payload[:version_string]} released: #{payload[:release_notes_url]}"
+  puts "This is what I tweet: #{twitter_message}"
+end
