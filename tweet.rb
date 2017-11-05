750a3ced7e7240a393f8bace0e9fb356d4246b51
Leif Gensert <leif@leif.io>
2017-11-05 17:28:29 +0000

    debug more

--- tweet.rb
+++ tweet.rb
@@ -1,13 +1,16 @@
 require "json"
 require "openssl"
-require "base64"
 
 data = ARGF.read.strip
 
 if data == ""
   $stderr.puts "Empty Payload"
-  return
+  return 1
 end
+
+$stdout.puts "====="
+$stdout.puts data.inspect
+$stdout.puts "====="
 
 type, signature = ENV["x_rp_webhook_signature"].split("=")
 secret = ENV["WEBHOOK_SECRET"]
@@ -25,4 +28,4 @@ version_info = payload[:version_info]
 base_version = "#{version_info[:major]}.#{version_info[:minor]}.#{version_info[:patch]}"
 twitter_message = "#{payload[:software][:name]} #{payload[:display_version]} released: #{payload[:release_notes_url]}"
 
-puts "I tweet: #{twitter_message}"
+$stdout.puts "I tweet: #{twitter_message}"
